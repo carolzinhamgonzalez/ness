c5dadeb03aa695f8a867bb84c3692cf5a9930e9d
carol <carolzinha.mgonzalez@gmail.com>
2019-02-01 14:35:01 -0200

    crud com erro

--- desafio/src/app/app.module.ts
+++ desafio/src/app/app.module.ts
@@ -3,38 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterService } from './services/register.service';
-import { RegisterComponent } from './register/register.component';
 
 // integração com firebase
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { FirebaseApp } from '@angular/fire/firebase.app.module';
+
 import { EditComponent } from './contatos/edit/edit.component';
 import { ListComponent } from './contatos/list/list.component';
-
-// import relacionado a style
-// import { ToastModule } from 'ng2-toastr';
-// import { ToastsManager } from 'ng2-toastr';
-// import { BrowserAnimationsModule } from '@angular/platform-browser';
-// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
     EditComponent,
     ListComponent
   ],
   imports: [
     BrowserModule,
-    // BrowserAnimationsModule,
-    // ToastModule.forRoot(),
-    // NoopAnimationsModule,
-
     FormsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment, FirebaseApp),
@@ -42,8 +28,7 @@ import { ListComponent } from './contatos/list/list.component';
     AppRoutingModule
 
   ],
-  // providers: [RegisterService, ToastsManager],
-  providers: [RegisterService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
